96a325dc934557ebd09dbce62fa839dce48d6245
KD <krishnadheeraj22@gmail.com>
2023-08-04 17:05:33 -0700

    Added method using case statements

--- Problem2.sql
+++ Problem2.sql
@@ -5,9 +5,20 @@
 -- Problems faced: None
 */
 
-
+--Method 1 Using XOR
 select coalesce(s2.id,s1.id) 'id', s1.student
 from Seat s1
     left join Seat s2
     on (s1.id +1) ^1 - 1 = s2.id
 order by id
+
+--Method 2 Using Case Statements
+select (
+    case 
+        when mod(id,2)!=0 and id != total then id+1
+        when mod(id,2)!=0 and id=total then id 
+        else id-1
+    end ) as id, student
+from Seat, ( select count(*) as total
+    from Seat) as seat_count
+order by id;
