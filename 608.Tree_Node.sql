61721f305d1454793129e8a19d4e3b040fe9ef91
Praveen Chandrasekaran <81367485+Praveen2795@users.noreply.github.com>
2023-11-08 09:46:31 -0500

    done with class 2 HW

---
+++ 608.Tree_Node.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select id, case when p_id is null then "Root"
+                when p_id in (select id from Tree) and
+                id in (select p_id from Tree) then "Inner"
+                else "Leaf" end as "type"
+from Tree
---
+++ 626.Exchange_Seats.sql
@@ -0,0 +1,2 @@
+select id, coalesce(case when id%2 = 0 then lag(student) over() else lead(student) over() end, student) as "student"
+from Seat
